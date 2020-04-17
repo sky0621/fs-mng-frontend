5fea5e2e978028e16444880951b79d836f4bde65
sky0621 <sky14work@gmail.com>
2020-04-18 05:00:28 +0900

    movie

---
+++ src/components/MovieForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-form>
+      <div>
+        Name: <v-text-field v-model="input.name"></v-text-field>
+      </div>
+      <div>
+        <video :src="uploadTargetUrl" width="320" height="180" controls></video>
+      </div>
+      <div>
+        <v-file-input v-model="input.movieFile" @change="getUploadTargetFileUrl" accept=".mp4"></v-file-input>
+      </div>
+      <div>
+        <v-btn @click="save()">登録</v-btn>
+      </div>
+    </v-form>
+  </v-container>
+</template>
+
+<script lang="ts">
+  import { Vue, Component } from 'nuxt-property-decorator'
+  import 'vue-apollo'
+  import createMovie from '@/apollo/mutations/movies.gql'
+
+  @Component({})
+  export default class MovieForm extends Vue {
+    private uploadTargetUrl: string = '';
+    getUploadTargetFileUrl(): void {
+
+    }
+  }
+</script>
---
+++ src/pages/new.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <MovieForm />
+  </div>
+</template>
+
+<script lang="ts">
+  import { Vue, Component } from 'nuxt-property-decorator'
+  import 'vue-apollo'
+  import MovieForm from '~/components/MovieForm.vue'
+
+  @Component({
+    components: { MovieForm }
+  })
+  export default class NewPage extends Vue {}
+</script>
